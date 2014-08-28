21a362f479ff69d0b409746e7f55e3e5a81488da
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2014-08-28 13:03:07 +0900

    fluentd-ui: install fluentd-ui by git tag, and install deps by bundler

---
+++ fluentd-ui/scripts/home/debian/fluentd-ui/start_server.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+if [ -f setenv.sh ]; then
+  . ./setenv.sh
+else
+  export SECRET_KEY_BASE=`bundle exec rake secret`
+  echo "export SECRET_KEY_BASE=$SECRET_KEY_BASE" > setenv.sh
+fi
+
+exec bundle exec rails s Puma -e production -p 9292
+
