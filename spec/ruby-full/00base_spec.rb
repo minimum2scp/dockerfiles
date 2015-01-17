ad3266312306ac059dea7e1e74a885bbecbb3de6
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2015-01-18 04:38:32 +0900

    use load instead of require

--- spec/ruby-full/00base_spec.rb
+++ spec/ruby-full/00base_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-require 'ruby/00base_spec'
-require 'ruby/01setup_spec'
-require 'ruby/02rbenv_spec'
-require 'ruby/03bundler-custom_spec'
+Dir["#{__dir__}/../ruby/*_spec.rb"].sort.each do |spec|
+  load spec
+end
+
--- spec/ruby/00base_spec.rb
+++ spec/ruby/00base_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
-require 'baseimage/00apt_spec'
-require 'baseimage/01setup_spec'
-require 'baseimage/02init-wrapper_spec'
+Dir["#{__dir__}/../baseimage/*_spec.rb"].sort.each do |spec|
+  load spec
+end
+
