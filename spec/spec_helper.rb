d18d3eaa5c23a3d77d00c411f870dd5858fa3de0
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2015-02-04 20:57:28 +0900

    Do not delete container in spec (workaround for Circle CI)
    test on previous commit failed on Circle CI:
    
    ```
      1) File "/etc/apt/sources.list" content
         Failure/Error: its(:content) { should match apt_line_re[false, 'deb',     'http://ftp.jp.debian.org/debian/', 'stable',         'main', 'non-free', 'contrib'] }
         Docker::Error::ServerError:
           Cannot destroy container e0f6531a3abbd0d8af1afb712d76af606feced8f37f8ac8a3a93c892d957f7be: Driver btrfs failed to remove root filesystem e0f6531a3abbd0d8af1afb712d76af606feced8f37f8ac8a3a93c892d957f7be: Failed to destroy btrfs snapshot: operation not permitted
    ```
    
    In Circle CI, `docker rm` failed:
    
    ```
    ubuntu@box400:~/dockerfiles$ docker rm 548bf23e48ae
    Error response from daemon: Cannot destroy container 548bf23e48ae: Driver btrfs failed to remove root filesystem 548bf23e48ae6b486222b8ed8be0a3b3854caea75f269058de3dd481f6a55228: Failed to destroy btrfs snapshot: operation not permitted
    FATA[0000] Error: failed to remove one or more containers
    ```

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,13 @@ require 'serverspec'
 
 set :backend, :docker
 set :docker_image, ENV['DOCKER_IMAGE']
+
+if ENV['CIRCLECI']
+  require 'docker'
+  class Docker::Container
+    def remove
+      # do not delete container
+    end
+    alias_method :delete, :remove
+  end
+end
