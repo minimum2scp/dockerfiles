880ca8f3f450fbd3653d3d3e1c26dc9f49e8c184
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2015-02-05 04:10:30 +0900

    skipped OS Detection

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,11 @@ require 'serverspec'
 set :backend, :docker
 set :docker_image, ENV['DOCKER_IMAGE']
 
+## skip OS detection
+## see specinfra-2.12.3/lib/specinfra/helper/os.rb
+##     specinfra-2.12.3/lib/specinfra/helper/detect_os/debian.rb
+set :os, :family => 'debian', :arch => 'x86_64', :release => '8.0'
+
 if ENV['CIRCLECI']
   require 'docker'
   class Docker::Container
@@ -12,3 +17,4 @@ if ENV['CIRCLECI']
     alias_method :delete, :remove
   end
 end
+
