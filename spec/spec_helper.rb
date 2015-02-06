5a04cc0db5f1d461adf0c0361de0a45a518e7cf5
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2015-02-06 21:52:37 +0900

    add comment for Circle CI workaround

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,8 @@ set :docker_image, ENV['DOCKER_IMAGE']
 ##     specinfra-2.12.3/lib/specinfra/helper/detect_os/debian.rb
 set :os, :family => 'debian', :arch => 'x86_64', :release => '8.0'
 
+## workaround for Circle CI
+## docker rm (removing btrfs snapshot) fails on Circle CI
 if ENV['CIRCLECI']
   require 'docker'
   class Docker::Container
