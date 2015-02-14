926ea8f530681d77c081f1df293007e02419ff6c
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2015-02-15 04:58:11 +0900

    ci_util.rb supports listing caches, deleting old caches
     * requires thor, sequel, sqlite3, text-table gems
     * stores cache meta data into sqlite3 database
     * breaks cache file naming rule
     * ignores .gitignore Rakefile README.md README.md.erb files in cache dependency

--- ci_util.rb
+++ ci_util.rb
@@ -2,100 +2,163 @@
 
 require 'digest/sha2'
 require 'find'
-require 'optparse'
 require 'pathname'
 require 'yaml'
 require 'bundler/setup'
+require 'thor'
 require 'docker'
-#require 'pry'
+require 'sequel'
+require 'text-table'
+require 'pry'
 
-def calc_deps(image_source_dir)
-  deps = {}
+CACHE_DIR = Pathname(ENV['CACHE_DIR'] || File.expand_path('~/.cache/docker'))
+CACHE_DIR.mkpath if !CACHE_DIR.directory?
+DB = Sequel.connect("sqlite://#{CACHE_DIR}/index.db")
+DB.create_table?(:image_caches) do
+  primary_key :id
+  String :image_name
+  String :image_source_dir
+  String :image_id
+  String :dependency
+  String :dependency_digest
+  Time :created_at
+  Time :last_used_at
+end
 
-  dockerfile=image_source_dir + 'Dockerfile'
-  if dockerfile.exist?
-    deps[:dockerfile_checksum] = Digest::SHA256.hexdigest(dockerfile.read)
-    from = dockerfile.read.scan(/^FROM (.+)$/).first.first
-    parent_image = ::Docker::Image.all.find{|img| img.info['RepoTags'].include?(from)}
-    deps[:parent] = {
-      :name => from,
-      :id => parent_image.id
-    }
-  end
+module CiUtil
+  class CLI < Thor
+    desc "pull IMAGE", "pull IMAGE and save cache, or load IMAGE from cache"
+    def pull(image)
+      with_cache(image) do
+        system("docker pull #{image}")
+      end
+    end
 
-  files = []
-  Dir.chdir(image_source_dir.to_s) do
-    if File.directory?('build')
-      Find.find('build') do |f|
-        next if File.directory?(f)
-        files << { :name => f, :checksum => Digest::SHA256.hexdigest(File.read(f)) }
+    desc "build IMAGE", "build IMAGE and save cache,  or load IMAGE from cache"
+    def build(image)
+      with_cache(image) do
+        Dir.chdir(image_source_dir(image).to_s) do
+          system("docker build -t #{image} .")
+        end
+      end
+    end
+
+    desc "gc", "gc old caches"
+    def gc(expire_seconds=3*24*60*60)
+      fields = ::CiUtil::ImageCache.show_fields
+      ::CiUtil::ImageCache.all do |cache|
+        if Time.now - cache.last_used_at > expire_seconds.to_i
+          puts "DELETE OLD CACHE: " + fields.map{ |k,v| "#{v}=#{cache.send(k)}" }.join(', ')
+          tarball = Pathname("#{::CACHE_DIR}/#{cache.tarball}")
+          tarball.unlink if tarball.exist?
+          cache.delete
+        end
+      end
+    end
+
+    desc "list", "list caches"
+    def list
+      image_caches = ::CiUtil::ImageCache.all
+      fields = ::CiUtil::ImageCache.show_fields
+      puts [ fields.values, *image_caches.map{|cache| fields.keys.map{|f| cache.send(f)}} ].to_table(first_row_is_head: true).to_s
+      puts ""
+      puts "(#{image_caches.size} caches)"
+    end
+
+    no_commands do
+      def with_cache(image)
+        deps = ::CiUtil::ImageDependency.new(image, image_source_dir(image))
+        cache = ::CiUtil::ImageCache.where(image_name:image, image_source_dir:image_source_dir(image).to_s, dependency_digest:deps.digest).first
+        if cache && Pathname("#{::CACHE_DIR}/#{cache.tarball}").readable?
+          puts "loading image cache from #{::CACHE_DIR}/#{cache.tarball}"
+          system("docker load -i #{::CACHE_DIR}/#{cache.tarball}")
+          cache.update(last_used_at: Time.now)
+        else
+          yield
+          cache = ::CiUtil::ImageCache.create(
+            image_name: image,
+            image_source_dir: image_source_dir(image),
+            image_id: image_id(image),
+            dependency: YAML.dump(deps.dependency),
+            dependency_digest: deps.digest,
+            created_at: Time.now,
+            last_used_at: Time.now
+          )
+          puts "saving image cache to #{::CACHE_DIR}/#{cache.tarball}"
+          system("docker save #{image} > #{::CACHE_DIR}/#{cache.tarball}")
+        end
+      end
+
+      def image_source_dir(image_name)
+        Pathname(image_name.split('/', 2).last.split(':').first)
+      end
+
+      def image_id(image_name)
+        ::Docker::Image.all.find{|img| img.info['RepoTags'].include?(image_name) }.id[0,10]
       end
     end
   end
-  deps[:files] = files.sort_by{|file_hash| file_hash[:name]}
 
-  deps
-end
-
-def cache_available?(image, cache_tarball, deps_file, image_source_dir, current_deps)
-  if !cache_tarball.readable? || !deps_file.readable?
-    return false
-  else
-    cached_deps = YAML.load(deps_file.read)
-    cached_deps == current_deps
-  end
-end
-
-def save_cache(image, cache_tarball, deps_file, image_source_dir, current_deps)
-  system("docker save #{image} > #{cache_tarball}")
-  deps_file.open('w') do |fh|
-    fh << YAML.dump(current_deps)
-  end
-end
-
-opts = {
-  :cache_dir => Pathname('~/.cache/docker').expand_path
-}
-ARGV.options do |q|
-  q.on('-c', '--cache-dir=DIR'){ |arg| opts[:cache_dir] = arg }
-  q.parse!
-end
-
-action = ARGV.shift
-image = ARGV.shift
-
-image_source_dir = Pathname('.') + image.split('/', 2).last.split(':').first
-current_deps = calc_deps(image_source_dir)
-current_deps_digest = Digest::SHA256.hexdigest(YAML.dump(current_deps))[0,10]
-cache_dir = Pathname(opts[:cache_dir])
-cache_tarball = cache_dir + "#{image_source_dir.basename}-#{current_deps_digest}.tar"
-deps_file = cache_dir + "#{image_source_dir.basename}-#{current_deps_digest}.deps"
-
-if !cache_dir.directory?
-  cache_dir.mkdir
-end
-
-case action
-when 'pull'
-  if cache_available?(image, cache_tarball, deps_file, image_source_dir, current_deps)
-    puts "loading image from #{cache_tarball}"
-    system("docker load -i #{cache_tarball}")
-  else
-    puts "cache is not available, starting pull ..."
-    system("docker pull #{image}")
-    puts "save cache..."
-    save_cache(image, cache_tarball, deps_file, image_source_dir, current_deps)
-  end
-when 'build'
-  if cache_available?(image, cache_tarball, deps_file, image_source_dir, current_deps)
-    puts "loading image from #{cache_tarball}"
-    system("docker load -i #{cache_tarball}")
-  else
-    puts "cache is not available, starting build ..."
-    Dir.chdir(image_source_dir.to_s) do
-      system("docker build -t #{image} .")
+  class ImageCache < Sequel::Model
+    def tarball
+      "#{self.image_source_dir}-#{self.image_id}.tar"
     end
-    puts "save cache..."
-    save_cache(image, cache_tarball, deps_file, image_source_dir, current_deps)
+
+    def show
+      Hash[self.class.show_fields.values.map{|f| [f, self.send(f)] }]
+    end
+
+    class << self
+      def show_fields
+        {
+          tarball:            'cached tarball',
+          image_name:         'image name',
+          image_source_dir:   'source dir',
+          dependency_digest:  'dependency digest',
+          created_at:         'created at',
+          last_used_at:       'last used at'
+        }
+      end
+    end
+  end
+
+  class ImageDependency
+    attr_accessor :image_name, :image_source_dir
+
+    def initialize(image_name, image_source_dir)
+      @image_name = image_name
+      @image_source_dir = image_source_dir
+    end
+
+    def dependency
+      if !@dependency
+        @dependency = { :image_name => @image_name }
+        dockerfile = @image_source_dir + 'Dockerfile'
+
+        if dockerfile.exist?
+          @dependency[:dockerfile_checksum] = Digest::SHA256.hexdigest(dockerfile.read)
+          from = dockerfile.read.scan(/^FROM (.+)$/).first.first
+          parent_image = ::Docker::Image.all.find{|img| img.info['RepoTags'].include?(from) }
+          @dependency[:parent] = { :name => from, :id => parent_image.id }
+        end
+
+        files = []
+        Dir.chdir(@image_source_dir.to_s) do
+          Dir.glob('**/*', File::FNM_DOTMATCH).each do |f|
+            next if File.directory?(f)
+            next if %w[.gitignore Rakefile README.md README.md.erb].include?(f)
+            files << { :name => f, :checksum => Digest::SHA256.hexdigest(File.read(f)) }
+          end
+        end
+        @dependency[:files] = files.sort_by{|file_hash| file_hash[:name]}
+      end
+      @dependency
+    end
+
+    def digest
+      @digest ||= Digest::SHA256.hexdigest(YAML.dump(self.dependency))
+    end
   end
 end
+
+CiUtil::CLI.start(ARGV)
