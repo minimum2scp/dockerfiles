f12aefca0c7591e0c29aa35fdbabde216d23c33b
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2015-06-11 23:44:49 +0900

    fixed indent

--- spec/ruby/00base_spec.rb
+++ spec/ruby/00base_spec.rb
@@ -17,9 +17,9 @@ describe 'minimum2scp/ruby' do
   #end
 
   %w[
-  ruby ruby-dev bundler rake pry
-  build-essential autoconf bison ca-certificates libgdbm-dev libncursesw5-dev libncurses5-dev libreadline6-dev tcl-dev tk-dev zlib1g-dev libssl-dev libffi-dev libyaml-dev libgmp-dev
-  gem2deb
+    ruby ruby-dev bundler rake pry
+    build-essential autoconf bison ca-certificates libgdbm-dev libncursesw5-dev libncurses5-dev libreadline6-dev tcl-dev tk-dev zlib1g-dev libssl-dev libffi-dev libyaml-dev libgmp-dev
+    gem2deb
   ].each do |pkg|
     describe package(pkg) do
       it { should be_installed }
@@ -27,12 +27,12 @@ describe 'minimum2scp/ruby' do
   end
 
   describe file('/usr/bin/ruby') do
-      it { should be_symlink }
-        it { should be_linked_to('ruby2.1') }
+    it { should be_symlink }
+    it { should be_linked_to('ruby2.1') }
   end
 
   describe command('ruby2.1 -v') do
-      its(:stdout) { should include 'ruby 2.1.5p273 (2014-11-13) [x86_64-linux-gnu]' }
+    its(:stdout) { should include 'ruby 2.1.5p273 (2014-11-13) [x86_64-linux-gnu]' }
   end
 
   describe file('/opt/rbenv') do
