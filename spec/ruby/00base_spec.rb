ace73e0ea1675e47d880a0838d4429eb5b80ba6f
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2016-07-15 00:01:42 +0900

    Fixed ruby spec (bundler package is updated)

--- spec/ruby/00base_spec.rb
+++ spec/ruby/00base_spec.rb
@@ -82,7 +82,7 @@ describe 'minimum2scp/ruby' do
     end
 
     describe package('bundler') do
-      it { should be_installed.with_version('1.12.5-2') }
+      it { should be_installed.with_version('1.12.5-3') }
     end
   end
 end
