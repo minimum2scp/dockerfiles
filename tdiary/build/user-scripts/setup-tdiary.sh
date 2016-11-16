8fdbfd34f438739ac8423a3be372c6840958755a
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2016-11-17 00:28:45 +0900

    tdiary: use ruby 2.3.2

--- tdiary/build/user-scripts/setup-tdiary.sh
+++ tdiary/build/user-scripts/setup-tdiary.sh
@@ -32,7 +32,7 @@ done
 ## run bundle install
 cd $GHQ_ROOT/github.com/tdiary/tdiary-core
 mkdir -p vendor/bundle
-bash -l -c "cd ~; rbenv local 2.3.1"
+bash -l -c "cd ~; rbenv local 2.3.2"
 bash -l -c "bundle install --path vendor/bundle --jobs=4"
 
 ## add Gemfile.local
