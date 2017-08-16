c76fb59f1c8b716fbb10e927144e47a19fc5f798
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2017-08-17 00:17:08 +0900

    rails5: updated nodejs version to 8.4.0

--- spec/rails5/00base_spec.rb
+++ spec/rails5/00base_spec.rb
@@ -79,12 +79,12 @@ describe 'minimum2scp/rails5' do
 
       describe command('nodenv version-name') do
         let(:login_shell){ true }
-        its(:stdout){ should eq "8.3.0\n" }
+        its(:stdout){ should eq "8.4.0\n" }
       end
 
       [
         {
-          nodejs: '8.3.0',
+          nodejs: '8.4.0',
         },
       ].each do |v|
           describe command('nodenv versions --bare --skip-aliases') do
