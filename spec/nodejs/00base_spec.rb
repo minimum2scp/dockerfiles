e629c17029a15a3d1c9106d29ef00452a91fdecc
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2017-11-09 00:12:18 +0900

    nodejs: updated nodejs version to 9.1.0

--- spec/nodejs/00base_spec.rb
+++ spec/nodejs/00base_spec.rb
@@ -64,12 +64,12 @@ describe 'minimum2scp/nodejs' do
 
     describe command('nodenv version-name') do
       let(:login_shell){ true }
-      its(:stdout){ should eq "9.0.0\n" }
+      its(:stdout){ should eq "9.1.0\n" }
     end
 
     [
       {
-        nodejs: '9.0.0',
+        nodejs: '9.1.0',
       },
     ].each do |v|
       describe command('nodenv versions --bare --skip-aliases') do
