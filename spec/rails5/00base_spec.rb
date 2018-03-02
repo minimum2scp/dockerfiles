c2b2e0bce5f03e658a0b2fbf446f943ce0bfc9e9
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2018-03-03 03:23:26 +0900

    rails5: updated nodejs version to 9.7.1

--- spec/rails5/00base_spec.rb
+++ spec/rails5/00base_spec.rb
@@ -79,12 +79,12 @@ describe 'minimum2scp/rails5' do
 
       describe command('nodenv version-name') do
         let(:login_shell){ true }
-        its(:stdout){ should eq "9.6.1\n" }
+        its(:stdout){ should eq "9.7.1\n" }
       end
 
       [
         {
-          nodejs: '9.6.1',
+          nodejs: '9.7.1',
         },
       ].each do |v|
           describe command('nodenv versions --bare --skip-aliases') do
