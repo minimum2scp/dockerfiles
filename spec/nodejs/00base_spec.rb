110092cf39638ecf60a4af8e3ed87009e66f7467
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2019-03-05 02:18:18 +0900

    nodejs: updated nodejs version to 11.10.1

--- spec/nodejs/00base_spec.rb
+++ spec/nodejs/00base_spec.rb
@@ -64,12 +64,12 @@ describe 'minimum2scp/nodejs' do
 
     describe command('nodenv version-name') do
       let(:login_shell){ true }
-      its(:stdout){ should eq "11.10.0\n" }
+      its(:stdout){ should eq "11.10.1\n" }
     end
 
     [
       {
-        nodejs: '11.10.0',
+        nodejs: '11.10.1',
       },
     ].each do |v|
       describe command('nodenv versions --bare --skip-aliases') do
