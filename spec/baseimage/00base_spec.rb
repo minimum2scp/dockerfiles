6b5a1e53fd3bc5114a28fc5aa17964e0469e33ea
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2019-09-20 03:05:59 +0900

    baseimage: docker-ce 19.03.2

--- spec/baseimage/00base_spec.rb
+++ spec/baseimage/00base_spec.rb
@@ -302,7 +302,7 @@ describe 'minimum2scp/baseimage' do
     end
 
     describe command('docker --version') do
-      its(:stdout){ should match a_string_starting_with('Docker version 19.03.1, ') }
+      its(:stdout){ should match a_string_starting_with('Docker version 19.03.2, ') }
     end
   end
 
