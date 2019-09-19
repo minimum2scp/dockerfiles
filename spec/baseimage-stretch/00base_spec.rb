64cde3a84ac8fef70024c938e2b76f0ba1061a4d
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2019-09-20 02:46:50 +0900

    baseimage, baseimage-buster, baseimage-stretch: docker-ce 19.03.2

--- spec/baseimage-stretch/00base_spec.rb
+++ spec/baseimage-stretch/00base_spec.rb
@@ -304,7 +304,7 @@ describe 'minimum2scp/baseimage-stretch' do
     end
 
     describe command('docker --version') do
-      its(:stdout){ should match a_string_starting_with('Docker version 19.03.1, ') }
+      its(:stdout){ should match a_string_starting_with('Docker version 19.03.2, ') }
     end
   end
 
