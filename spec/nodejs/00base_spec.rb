993292bd4a2241611b28ab3d7db5f7efbd0e5aa4
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2020-01-11 00:11:14 +0900

    nodejs: updated nodejs version to 13.6.0

--- spec/nodejs/00base_spec.rb
+++ spec/nodejs/00base_spec.rb
@@ -64,12 +64,12 @@ describe 'minimum2scp/nodejs' do
 
     describe command('nodenv version-name') do
       let(:login_shell){ true }
-      its(:stdout){ should eq "13.5.0\n" }
+      its(:stdout){ should eq "13.6.0\n" }
     end
 
     [
       {
-        nodejs: '13.5.0',
+        nodejs: '13.6.0',
       },
     ].each do |v|
       describe command('nodenv versions --bare --skip-aliases') do
