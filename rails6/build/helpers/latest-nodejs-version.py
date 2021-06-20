6806735150812a3443ddca053a0e455dfcbbcea1
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2021-06-21 00:06:46 +0900

    nodejs, rails6: fixed style checks for auto-detect latest nodejs version script
    ```
    % flake8 nodejs/build/helpers/latest-nodejs-version.py rails6/build/helpers/latest-nodejs-version.py
    nodejs/build/helpers/latest-nodejs-version.py:3:11: E401 multiple imports on one line
    nodejs/build/helpers/latest-nodejs-version.py:4:1: F401 'urllib.request.Request' imported but unused
    nodejs/build/helpers/latest-nodejs-version.py:4:1: F401 'urllib.request.urlopen' imported but unused
    nodejs/build/helpers/latest-nodejs-version.py:10:3: E111 indentation is not a multiple of 4
    nodejs/build/helpers/latest-nodejs-version.py:12:13: E201 whitespace after '['
    nodejs/build/helpers/latest-nodejs-version.py:12:80: E501 line too long (101 > 79 characters)
    nodejs/build/helpers/latest-nodejs-version.py:12:100: E202 whitespace before ']'
    nodejs/build/helpers/latest-nodejs-version.py:17:1: W391 blank line at end of file
    rails6/build/helpers/latest-nodejs-version.py:3:11: E401 multiple imports on one line
    rails6/build/helpers/latest-nodejs-version.py:4:1: F401 'urllib.request.Request' imported but unused
    rails6/build/helpers/latest-nodejs-version.py:4:1: F401 'urllib.request.urlopen' imported but unused
    rails6/build/helpers/latest-nodejs-version.py:10:3: E111 indentation is not a multiple of 4
    rails6/build/helpers/latest-nodejs-version.py:12:13: E201 whitespace after '['
    rails6/build/helpers/latest-nodejs-version.py:12:80: E501 line too long (101 > 79 characters)
    rails6/build/helpers/latest-nodejs-version.py:12:100: E202 whitespace before ']'
    rails6/build/helpers/latest-nodejs-version.py:17:1: W391 blank line at end of file
    ```

--- rails6/build/helpers/latest-nodejs-version.py
+++ rails6/build/helpers/latest-nodejs-version.py
@@ -1,17 +1,19 @@
 #! /usr/bin/python3
 
-import sys, json, re, urllib.request
-from urllib.request import Request, urlopen
+import sys
+import json
+import re
+import urllib.request
 from distutils.version import StrictVersion
 
 url = 'https://nodejs.org/download/release/index.json'
 req = urllib.request.Request(url)
 with urllib.request.urlopen(req) as res:
-  index = json.load(res)
+    index = json.load(res)
 
-versions = [ re.sub(r"^v", "", v["version"]) for v in index if v["version"].startswith(sys.argv[1]) ]
+versions = [re.sub(r"^v", "", v["version"])
+            for v in index if v["version"].startswith(sys.argv[1])]
 versions.sort(key=lambda v: StrictVersion(v), reverse=True)
 latest = versions[0]
 
 print(latest)
-
