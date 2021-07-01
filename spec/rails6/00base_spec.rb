ba6611669901003accf30e75cdc7ef977a3b0f38
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2021-07-02 02:29:48 +0900

    rails6: updated nodejs version to 16.4.1

--- spec/rails6/00base_spec.rb
+++ spec/rails6/00base_spec.rb
@@ -79,12 +79,12 @@ describe 'minimum2scp/rails6' do
 
       describe command('nodenv version-name') do
         let(:login_shell){ true }
-        its(:stdout){ should eq "16.4.0\n" }
+        its(:stdout){ should eq "16.4.1\n" }
       end
 
       [
         {
-          nodejs: '16.4.0',
+          nodejs: '16.4.1',
         },
       ].each do |v|
           describe command('nodenv versions --bare --skip-aliases') do
@@ -103,15 +103,15 @@ describe 'minimum2scp/rails6' do
       let(:login_shell){ true }
       its(:stdout){
         should eq <<~ALIASES
-          16 => 16.4.0
-          16.4 => 16.4.0
+          16 => 16.4.1
+          16.4 => 16.4.1
         ALIASES
       }
     end
 
     {
-      '16' => '16.4.0',
-      '16.4' => '16.4.0',
+      '16' => '16.4.1',
+      '16.4' => '16.4.1',
     }.each do |src, dest|
       describe file("/opt/nodenv/versions/#{src}") do
         it { should be_symlink }
