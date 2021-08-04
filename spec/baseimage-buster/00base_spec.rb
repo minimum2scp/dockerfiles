3ba358a99c44f14c6b9dfbfe7bb75e4651431859
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2021-08-05 00:07:28 +0900

    baseimage, baseimage-bullseye, baseimage-buster, baseimage-stretch: docker-ce 20.10.8

--- spec/baseimage-buster/00base_spec.rb
+++ spec/baseimage-buster/00base_spec.rb
@@ -302,7 +302,7 @@ describe 'minimum2scp/baseimage-buster' do
     end
 
     describe command('docker --version') do
-      its(:stdout){ should start_with('Docker version 20.10.7, ') }
+      its(:stdout){ should start_with('Docker version 20.10.8, ') }
     end
   end
 
