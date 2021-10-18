75b106d83be39863b0a80f80a8d3bc60ed1e011d
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2021-10-19 02:41:17 +0900

    ruby: fixed spec
    rubygems 3.2.27-2 and bundler 2.2.27-2 was uploaded to sid
    https://tracker.debian.org/news/1268217/accepted-rubygems-3227-2-source-into-unstable/

--- spec/ruby/00base_spec.rb
+++ spec/ruby/00base_spec.rb
@@ -88,7 +88,7 @@ describe 'minimum2scp/ruby' do
     end
 
     describe package('bundler') do
-      it { should be_installed.with_version('2.2.27-1') }
+      it { should be_installed.with_version('2.2.27-2') }
     end
   end
 end
