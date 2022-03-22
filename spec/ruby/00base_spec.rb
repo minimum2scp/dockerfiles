f9b100a3b291f766c91a516531c4601027a4668f
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2022-03-23 00:11:37 +0900

    ruby, ruby-full: fixed spec (ruby 3.0.3)

--- spec/ruby/00base_spec.rb
+++ spec/ruby/00base_spec.rb
@@ -43,7 +43,7 @@ describe 'minimum2scp/ruby' do
     end
 
     describe command('ruby3.0 -v') do
-      its(:stdout) { should start_with('ruby 3.0.2p') }
+      its(:stdout) { should start_with('ruby 3.0.3p') }
     end
 
     describe file('/opt/rbenv') do
