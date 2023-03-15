dd0e20df6ca2464e5b20007e9af0acadcd7d9db0
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2023-03-16 00:01:46 +0900

    baseimage: nginx 1.22.1-9
    https://tracker.debian.org/news/1426966/accepted-nginx-1221-8-source-into-unstable/
    https://tracker.debian.org/news/1426987/accepted-nginx-1221-9-source-into-unstable/

--- spec/baseimage/00base_spec.rb
+++ spec/baseimage/00base_spec.rb
@@ -360,7 +360,7 @@ describe 'minimum2scp/baseimage' do
         pending 'Switched nginx package to debian.org ones'
         should be_installed.with_version('1.22.1-1~bullseye')
       }
-      it { should be_installed.with_version('1.22.1-7') }
+      it { should be_installed.with_version('1.22.1-9') }
     end
 
     describe file('/etc/nginx/conf.d/misc.conf') do
