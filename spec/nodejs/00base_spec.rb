833dce45af4de6df65a7399c1eb3039360cc622e
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2023-05-01 00:01:17 +0900

    nodejs: updated nodejs version to 20.0.0

--- spec/nodejs/00base_spec.rb
+++ spec/nodejs/00base_spec.rb
@@ -64,12 +64,12 @@ describe 'minimum2scp/nodejs' do
 
     describe command('nodenv version-name') do
       let(:login_shell){ true }
-      its(:stdout){ should eq "18.16.0\n" }
+      its(:stdout){ should eq "20.0.0\n" }
     end
 
     [
       {
-        nodejs: '18.16.0',
+        nodejs: '20.0.0',
       },
     ].each do |v|
       describe command('nodenv versions --bare --skip-aliases') do
@@ -87,15 +87,15 @@ describe 'minimum2scp/nodejs' do
       let(:login_shell){ true }
       its(:stdout){
         should eq <<~ALIASES
-          18 => 18.16.0
-          18.16 => 18.16.0
+          20 => 20.0.0
+          20.0 => 20.0.0
         ALIASES
       }
     end
 
     {
-      '18' => '18.16.0',
-      '18.16' => '18.16.0',
+      '20' => '20.0.0',
+      '20.0' => '20.0.0',
     }.each do |src, dest|
       describe file("/opt/nodenv/versions/#{src}") do
         it { should be_symlink }
