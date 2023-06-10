d092a16777f1d2f14d19ece522fb95bf85f432c5
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2023-06-11 00:04:16 +0900

    rails7: updated nodejs version to 20.3.0

--- spec/rails7/00base_spec.rb
+++ spec/rails7/00base_spec.rb
@@ -78,12 +78,12 @@ describe 'minimum2scp/rails7' do
 
       describe command('nodenv version-name') do
         let(:login_shell){ true }
-        its(:stdout){ should eq "20.2.0\n" }
+        its(:stdout){ should eq "20.3.0\n" }
       end
 
       [
         {
-          nodejs: '20.2.0',
+          nodejs: '20.3.0',
         },
       ].each do |v|
           describe command('nodenv versions --bare --skip-aliases') do
@@ -102,15 +102,15 @@ describe 'minimum2scp/rails7' do
       let(:login_shell){ true }
       its(:stdout){
         should eq <<~ALIASES
-          20 => 20.2.0
-          20.2 => 20.2.0
+          20 => 20.3.0
+          20.3 => 20.3.0
         ALIASES
       }
     end
 
     {
-      '20' => '20.2.0',
-      '20.2' => '20.2.0',
+      '20' => '20.3.0',
+      '20.3' => '20.3.0',
     }.each do |src, dest|
       describe file("/opt/nodenv/versions/#{src}") do
         it { should be_symlink }
