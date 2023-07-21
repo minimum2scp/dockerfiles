4da5cfa6464c0c56d2b752fa1dec8333d7615f7f
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2023-07-22 00:02:05 +0900

    Added systemd-bookworm image

---
+++ spec/systemd-bookworm/00base_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'minimum2scp/systemd-bookworm' do
+  context 'without env' do
+    before(:all) do
+      start_container({
+        'Image' => ENV['DOCKER_IMAGE'] || "minimum2scp/#{File.basename(__dir__)}:latest",
+        'HostConfig' => { 'Privileged' => true },
+      })
+    end
+
+    after(:all) do
+      stop_container
+    end
+
+    %w[
+      systemd systemd-sysv dbus libpam-systemd less
+    ].each do |pkg|
+      describe package(pkg) do
+        it { should be_installed }
+      end
+    end
+  end
+end
