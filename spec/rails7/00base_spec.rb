f4abf6969686d03c1f596f0a7096e6f8fc827ae2
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2024-07-20 00:01:34 +0900

    rails7: install latest ruby-binary (ruby 3.2.4 / rubygems 3.5.16, bundler 2.5.16)

--- spec/rails7/00base_spec.rb
+++ spec/rails7/00base_spec.rb
@@ -33,10 +33,10 @@ describe 'minimum2scp/rails7' do
 
       describe command('rbenv version-name') do
         let(:login_shell){ true }
-        its(:stdout){ should eq "3.2.2\n" }
+        its(:stdout){ should eq "3.2.4\n" }
       end
 
-      describe command("RBENV_VERSION=3.2.2 gem list") do
+      describe command("RBENV_VERSION=3.2.4 gem list") do
         let(:login_shell){ true }
         its(:stdout){ should match /^rails \(7(?:\.\d+){2,3}\)$/ }
       end
