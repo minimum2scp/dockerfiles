5276256c7719fec0982739150ae4a4e1d252d6d4
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2024-10-08 00:01:52 +0900

    baseimage: update nginx package version spec

--- spec/baseimage/00base_spec.rb
+++ spec/baseimage/00base_spec.rb
@@ -373,7 +373,7 @@ describe 'minimum2scp/baseimage' do
         pending 'Switched nginx package to debian.org ones'
         should be_installed.with_version('1.22.1-1~bullseye')
       }
-      it { should be_installed.with_version('1.26.0-2') }
+      it { should be_installed.with_version('1.26.0-3') }
     end
 
     describe file('/etc/nginx/conf.d/misc.conf') do
