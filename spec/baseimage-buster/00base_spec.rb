f72c2e125081a83e1454eec255da363b633c0ffb
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2024-12-20 00:10:35 +0900

    baseimage, baseimage-bookworm, baseimage-bullseye, baseimage-buster, baseimage-stretch: docker-ce 27.4.0

--- spec/baseimage-buster/00base_spec.rb
+++ spec/baseimage-buster/00base_spec.rb
@@ -322,7 +322,7 @@ describe 'minimum2scp/baseimage-buster' do
     end
 
     describe command('docker --version') do
-      its(:stdout){ should start_with('Docker version 24.0.7, ') }
+      its(:stdout){ should start_with('Docker version 27.4.0, ') }
     end
   end
 
