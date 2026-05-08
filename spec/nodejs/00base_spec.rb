9736266e3033d82541c85b8a25599365cfb99d07
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2026-05-08 23:44:12 +0900

    nodejs: updated nodejs version to 26.1.0

--- spec/nodejs/00base_spec.rb
+++ spec/nodejs/00base_spec.rb
@@ -63,12 +63,12 @@ describe 'minimum2scp/nodejs' do
 
     describe command('nodenv version-name') do
       let(:login_shell){ true }
-      its(:stdout){ should eq "24.15.0\n" }
+      its(:stdout){ should eq "26.1.0\n" }
     end
 
     [
       {
-        nodejs: '24.15.0',
+        nodejs: '26.1.0',
       },
     ].each do |v|
       describe command('nodenv versions --bare --skip-aliases') do
@@ -86,15 +86,15 @@ describe 'minimum2scp/nodejs' do
       let(:login_shell){ true }
       its(:stdout){
         should eq <<~ALIASES
-          24 => 24.15.0
-          24.15 => 24.15.0
+          26 => 26.1.0
+          26.1 => 26.1.0
         ALIASES
       }
     end
 
     {
-      '24' => '24.15.0',
-      '24.15' => '24.15.0',
+      '26' => '26.1.0',
+      '26.1' => '26.1.0',
     }.each do |src, dest|
       describe file("/opt/nodenv/versions/#{src}") do
         it { should be_symlink }
